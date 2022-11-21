44a51231b02b4d371773d6390c77f4cc96515aa0
김상두 <puki4416@gmail.com>
2022-11-21 21:57:44 +0900

    test : usePreventBack hook 테스트

---
+++ src/__tests__/customHook/usePreventBack.ts
@@ -0,0 +1,44 @@
+import { renderHook } from "@testing-library/react";
+import usePreventBack from "../../lib/customHook/usePreventBack";
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+window.history.pushState = jest.fn();
+window.addEventListener = jest.fn();
+window.removeEventListener = jest.fn();
+
+describe("usePreventBack Hook 테스트", () => {
+  it("초기 세팅", () => {
+    const target = "/quiz";
+    renderHook(() => usePreventBack({ target }));
+    expect(window.history.pushState).toBeCalledWith(null, "", target);
+    expect(window.addEventListener).toBeCalledWith(
+      "popstate",
+      expect.any(Function)
+    );
+  });
+  it("뒤로가기 버튼을 눌렀을때 확인을 누른경우", () => {
+    window.confirm = jest.fn().mockReturnValue(1);
+    const target = "/quiz";
+    const { unmount, result } = renderHook(() => usePreventBack({ target }));
+    result.current.preventGoBack();
+    unmount();
+    expect(mockedUsedNavigate).toBeCalledWith(-1);
+    expect(window.removeEventListener).toBeCalledWith(
+      "popstate",
+      expect.any(Function)
+    );
+  });
+  it("뒤로가기 버튼을 눌렀을때 거절을 누른경우", () => {
+    window.confirm = jest.fn().mockReturnValue(0);
+    const target = "/quiz";
+    const { result } = renderHook(() => usePreventBack({ target }));
+    result.current.preventGoBack();
+    expect(window.history.pushState).toBeCalledWith(null, "", target);
+    expect(window.history.pushState).toBeCalledTimes(2);
+  });
+});
