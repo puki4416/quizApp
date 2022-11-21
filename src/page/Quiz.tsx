9871c6ae8f3b3846bd95b3ae38ea31f7487d121a
김상두 <puki4416@gmail.com>
2022-11-21 18:21:38 +0900

    feat : Quiz page 구현

---
+++ src/page/Quiz.tsx
@@ -0,0 +1,13 @@
+import Header from "../component/Common/Header/Header";
+import Body from "../component/Quiz/Body/Body";
+
+const Quiz = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default Quiz;
