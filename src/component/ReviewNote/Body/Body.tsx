794056048618934e76db9cc302a5b983bf3a1df5
김상두 <puki4416@gmail.com>
2022-11-21 18:19:54 +0900

    ReviewNote/Body 컴포넌트 구현 및 useReview Hook 구현

---
+++ src/component/ReviewNote/Body/Body.tsx
@@ -0,0 +1,32 @@
+import useReview from "../../../lib/customHook/useReview";
+import ProblemInfo from "../ProblemInfo/ProblemInfo";
+import QuestionAnswer from "../QuestionAnswer/QuestionAnswer";
+import styles from "./Body.module.css";
+
+const Body = () => {
+  const reviews = useReview();
+
+  return (
+    <div className={styles.mainBlock}>
+      <div className={styles.title}>오답노트</div>
+      {reviews.map((review) => {
+        return (
+          <div key={review.question} className={styles.reviewBlock}>
+            <ProblemInfo
+              category={review.category}
+              difficulty={review.difficulty}
+              count={review.count}
+            />
+            <QuestionAnswer
+              question={review.question}
+              answers={review.answers}
+              correctAnswer={review.correctAnswer}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/lib/customHook/useReview.ts
@@ -0,0 +1,18 @@
+import { useLayoutEffect, useState } from "react";
+import { QuizContentState } from "../../store/quizList/reducer";
+
+interface QuizReview extends QuizContentState {
+  count: string;
+}
+
+const useReview = () => {
+  const [reviews, setReviews] = useState<QuizReview[]>([]);
+  useLayoutEffect(() => {
+    const output = localStorage.getItem("wrongQuiz");
+    const arr: QuizReview[] = output !== null ? JSON.parse(output) : [];
+    setReviews(arr);
+  }, []);
+  return reviews;
+};
+
+export default useReview;
