71d2e1c15bfa1b94f690da0598d9afb95911f25a
김상두 <puki4416@gmail.com>
2022-11-21 21:58:55 +0900

    test : useSaveWrong hook 테스트

---
+++ src/__tests__/customHook/useSaveWrong.tsx
@@ -0,0 +1,83 @@
+import { renderHook } from "@testing-library/react";
+import useSaveWrong from "../../lib/customHook/useSaveWrong";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import { ReactNode } from "react";
+import { QuizContentState } from "../../store/quizList/reducer";
+
+class LocalStorageMock {
+  store: { [index: string]: string };
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+window.localStorage = new LocalStorageMock() as unknown as Storage;
+
+const mockStore = configureMockStore()();
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <Provider store={mockStore}>{children}</Provider>
+);
+
+describe("useReview Hook 테스트", () => {
+  it("local스토리지에 중복된 오답이 없는 경우", () => {
+    renderHook(
+      () =>
+        useSaveWrong({
+          wrongNumbers: [1],
+          quizList: new Array(4)
+            .fill(0)
+            .map(
+              (_, index) =>
+                ({ question: `test${index}` } as unknown as QuizContentState)
+            ),
+        }),
+      { wrapper }
+    );
+    expect(localStorage.getItem("wrongQuiz")).toEqual(
+      JSON.stringify([{ question: "test1", count: 1 }])
+    );
+    expect(mockStore.getActions()[0]).toEqual({
+      payload: undefined,
+      type: "quizList/initializeQuizList",
+    });
+  });
+  it("local스토리지에 중복된 오답이 있는 경우", () => {
+    renderHook(
+      () =>
+        useSaveWrong({
+          wrongNumbers: [1],
+          quizList: new Array(4)
+            .fill(0)
+            .map(
+              (_, index) =>
+                ({ question: `test${index}` } as unknown as QuizContentState)
+            ),
+        }),
+      { wrapper }
+    );
+    expect(localStorage.getItem("wrongQuiz")).toEqual(
+      JSON.stringify([{ question: "test1", count: 2 }])
+    );
+    expect(mockStore.getActions()[1]).toEqual({
+      payload: undefined,
+      type: "quizList/initializeQuizList",
+    });
+  });
+});
