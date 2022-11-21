280ae9cb77839cb370c0bd1c1b52b00b91362423
김상두 <puki4416@gmail.com>
2022-11-21 18:30:05 +0900

    feat : ReviewNote page 구현

---
+++ src/page/ReviewNote.tsx
@@ -0,0 +1,13 @@
+import Header from "../component/Common/Header/Header";
+import Body from "../component/ReviewNote/Body/Body";
+
+const ReviewNote = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default ReviewNote;
