39dfee0db07390237c9d575ab11237d04a40bb69
김상두 <puki4416@gmail.com>
2022-11-21 21:56:50 +0900

    test : useAnswer hook 테스트

---
+++ src/__tests__/customHook/useAnswer.tsx
@@ -0,0 +1,53 @@
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import { ReactNode } from "react";
+import useAnswer from "../../lib/customHook/useAnswer";
+import { act, renderHook } from "@testing-library/react";
+
+describe("useAnswer Hook 테스트", () => {
+  const mockStore = configureMockStore()();
+  const wrapper = ({ children }: { children: ReactNode }) => (
+    <Provider store={mockStore}>{children}</Provider>
+  );
+  it("정답이 일치하는 경우", async () => {
+    const { result } = renderHook(
+      () =>
+        useAnswer({
+          status: "Progress",
+          correctAnswer: "test1",
+          order: 1,
+        }),
+      {
+        wrapper,
+      }
+    );
+    act(() => {
+      result.current("test1");
+    });
+    expect(mockStore.getActions()[0]).toEqual({
+      payload: undefined,
+      type: "quizStatus/setSuccess",
+    });
+  });
+
+  it("정답이 불일치 하는경우", async () => {
+    const { result } = renderHook(
+      () =>
+        useAnswer({
+          status: "Progress",
+          correctAnswer: "test1",
+          order: 1,
+        }),
+      {
+        wrapper,
+      }
+    );
+    act(() => {
+      result.current("test2");
+    });
+    expect(mockStore.getActions()[1]).toEqual({
+      payload: 1,
+      type: "quizStatus/setFail",
+    });
+  });
+});
