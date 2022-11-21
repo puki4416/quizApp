d0f35852837c65b4a5ca549ad2d4ec1a49b39a9b
김상두 <puki4416@gmail.com>
2022-11-21 18:18:38 +0900

    feat : ReviewNote/ProblemInfo 컴포넌트 구현

---
+++ src/component/ReviewNote/ProblemInfo/ProblemInfo.tsx
@@ -0,0 +1,19 @@
+import styles from "./ProblemInfo.module.css";
+
+interface InfoProps {
+  category: string;
+  difficulty: string;
+  count: string;
+}
+
+const ProblemInfo = ({ category, difficulty, count }: InfoProps) => {
+  return (
+    <ul className={styles.mainBlock}>
+      <li>카테고리 : {category}</li>
+      <li>난이도 : {difficulty}</li>
+      <li>오답횟수 : {count}</li>
+    </ul>
+  );
+};
+
+export default ProblemInfo;
