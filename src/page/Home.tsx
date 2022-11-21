6e56d3e9c923032f83b5fc273abad6db33b11c30
김상두 <puki4416@gmail.com>
2022-11-21 18:21:04 +0900

    feat : Home page 구현

---
+++ src/page/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../component/Common/Header/Header";
+import Body from "../component/Home/Body/Body";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default Home;
