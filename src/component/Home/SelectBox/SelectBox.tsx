998203546c11cc094befc85b10b32323ca6a350a
김상두 <puki4416@gmail.com>
2022-11-21 17:11:23 +0900

    feat : SelectBox 컴포넌트 구현

---
+++ src/component/Home/SelectBox/SelectBox.tsx
@@ -0,0 +1,39 @@
+import styles from "./SelectBox.module.css";
+
+interface SelectBoxProps {
+  title: string;
+  optionData: {
+    value: string;
+    viewData: string;
+  }[];
+  defaultValue: string;
+  selectRef: React.RefObject<HTMLSelectElement>;
+}
+
+const SelectBox = ({
+  title,
+  optionData,
+  defaultValue,
+  selectRef,
+}: SelectBoxProps) => {
+  return (
+    <div>
+      <h3 className={styles.title}>{title}</h3>
+      <select
+        defaultValue={defaultValue}
+        className={styles.select}
+        ref={selectRef}
+      >
+        {optionData.map(({ value, viewData }) => {
+          return (
+            <option value={value} key={value}>
+              {viewData}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectBox;
