3df869002c0b309b6596d9edb00ce62e2d89e523
김상두 <puki4416@gmail.com>
2022-11-21 18:23:24 +0900

    feat : Result page 구현

---
+++ src/page/Result.tsx
@@ -0,0 +1,13 @@
+import Header from "../component/Common/Header/Header";
+import Body from "../component/Result/Body/Body";
+
+const Result = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+export default Result;
