f77684d83f13f1fdc27783d9de99e5210da60d16
김상두 <puki4416@gmail.com>
2022-11-21 17:09:51 +0900

    feat : Home/Loading 컴포넌트 구현

---
+++ src/component/Home/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import styles from "./Loading.module.css";
+import spinner from "../../../imgs/spinner.gif";
+
+interface LoadingProps {
+  active: boolean;
+}
+
+const Loading = ({ active }: LoadingProps) => {
+  if (active) {
+    return (
+      <div className={styles.mainBlock}>
+        <img src={spinner} alt="spinner" className={styles.spinner} />
+        <div className={styles.text}>퀴즈 문제를 생성중입니다</div>
+      </div>
+    );
+  }
+  return null;
+};
+
+export default Loading;
