0d7ac45e62d2017bf908a4ee2b3f0477cbacf17d
김상두 <puki4416@gmail.com>
2022-11-22 17:35:45 +0900

    test : useReview hook 기능 변경에 따른 테스트 코드 수정

--- src/__tests__/customHook/useReview.ts
+++ src/__tests__/customHook/useReview.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import useReview from "../../lib/customHook/useReview";
 
 class LocalStorageMock {
@@ -29,11 +29,101 @@ window.localStorage = new LocalStorageMock() as unknown as Storage;
 describe("useReview Hook 테스트", () => {
   it("local스토리지에서 가져온 값이 없는경우", () => {
     const { result } = renderHook(() => useReview());
-    expect(result.current).toEqual([]);
+    expect(result.current.reviews).toEqual([]);
   });
+
   it("local스토리지에서 가져온 값이 있는경우", () => {
-    window.localStorage.setItem("wrongQuiz", JSON.stringify(["a", "b", "c"]));
+    window.localStorage.setItem(
+      "wrongQuiz",
+      JSON.stringify([
+        { date: new Date(2022, 3, 1), answers: [1, 2, 3, 4] },
+        { date: new Date(2022, 2, 1), answers: [1, 2, 3, 4] },
+        { date: new Date(2022, 1, 1), answers: [1, 2, 3, 4] },
+      ])
+    );
     const { result } = renderHook(() => useReview());
-    expect(result.current).toEqual(["a", "b", "c"]);
+    expect(result.current.reviews).toEqual([
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 3, 1))),
+        answers: [1, 2, 3, 4],
+      },
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 2, 1))),
+        answers: [1, 2, 3, 4],
+      },
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 1, 1))),
+        answers: [1, 2, 3, 4],
+      },
+    ]);
+  });
+
+  it("최신순으로 정렬하는 경우", () => {
+    window.localStorage.setItem(
+      "wrongQuiz",
+      JSON.stringify([
+        { date: new Date(2022, 3, 1), answers: [1, 2, 3, 4] },
+        { date: new Date(2022, 2, 1), answers: [1, 2, 3, 4] },
+        { date: new Date(2022, 1, 1), answers: [1, 2, 3, 4] },
+      ])
+    );
+    const { result } = renderHook(() => useReview());
+    act(() => {
+      result.current.changeSort("recent");
+    });
+    expect(result.current.reviews).toEqual([
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 3, 1))),
+        answers: [1, 2, 3, 4],
+      },
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 2, 1))),
+        answers: [1, 2, 3, 4],
+      },
+      {
+        date: JSON.parse(JSON.stringify(new Date(2022, 1, 1))),
+        answers: [1, 2, 3, 4],
+      },
+    ]);
+  });
+
+  it("오답횟수순 으로 정렬하는 경우", () => {
+    window.localStorage.setItem(
+      "wrongQuiz",
+      JSON.stringify([
+        { count: 1, answers: [1, 2, 3, 4] },
+        { count: 2, answers: [1, 2, 3, 4] },
+        { count: 3, answers: [1, 2, 3, 4] },
+      ])
+    );
+    const { result } = renderHook(() => useReview());
+    act(() => {
+      result.current.changeSort("manyCount");
+    });
+    expect(result.current.reviews).toEqual([
+      { count: 3, answers: [1, 2, 3, 4] },
+      { count: 2, answers: [1, 2, 3, 4] },
+      { count: 1, answers: [1, 2, 3, 4] },
+    ]);
+  });
+
+  it("쉬운 난이도 으로 정렬하는 경우", () => {
+    window.localStorage.setItem(
+      "wrongQuiz",
+      JSON.stringify([
+        { difficulty: "hard", answers: [1, 2, 3, 4] },
+        { difficulty: "medium", answers: [1, 2, 3, 4] },
+        { difficulty: "easy", answers: [1, 2, 3, 4] },
+      ])
+    );
+    const { result } = renderHook(() => useReview());
+    act(() => {
+      result.current.changeSort("difficulty");
+    });
+    expect(result.current.reviews).toEqual([
+      { difficulty: "easy", answers: [1, 2, 3, 4] },
+      { difficulty: "medium", answers: [1, 2, 3, 4] },
+      { difficulty: "hard", answers: [1, 2, 3, 4] },
+    ]);
   });
 });
