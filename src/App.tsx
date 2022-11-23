ad6ee7bf8532a2c90721eb8bbdd49d09fad326ef
김상두 <puki4416@gmail.com>
2022-11-23 16:14:12 +0900

    fix : 로딩이미지가 늦게 출력되는 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,14 @@ import Result from "./page/Result";
 import ReviewNote from "./page/ReviewNote";
 import PrivateRoute from "./privateRoute";
 import { ReducerType } from "./store";
+import "../src/imgs/spinner.gif";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const img: HTMLImageElement = new Image();
+    img.src = "./spinner.gif";
+  }, []);
   const result = useSelector((state: ReducerType) => state.quizResult);
   const quizList = useSelector((state: ReducerType) => state.quizList.content);
   return (
